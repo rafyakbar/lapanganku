299081aba130d172c76461e95697ed3b12fe75c7
Rafy Aulia Akbar <rafy683@gmail.com>
2018-02-25 15:46:32 +0700

    tabel lapangan

--- database/migrations/2018_02_24_162050_lapangan_table.php
+++ database/migrations/2018_02_24_162050_lapangan_table.php
@@ -13,7 +13,16 @@ class LapanganTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('lapangan', function (Blueprint $table){
+            $table->bigIncrements('id');
+            $table->bigInteger('sportcenter_id')->unsigned();
+            $table->foreign('sportcenter_id')->references('id')->on('sportcenter')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->integer('jenis_id')->unsigned();
+            $table->foreign('jenis_id')->references('id')->on('jenis')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->bigInteger('harga_id')->unsigned();
+            $table->foreign('harga_id')->references('id')->on('harga')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->text('keterangan');
+        });
     }
 
     /**
