7d12244319cc60c599e35a5aaed86bd250543556
Rafy Aulia Akbar <rafy683@gmail.com>
2018-02-28 16:58:50 +0700

    model Kelurahan

---
+++ app/Kelurahan.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kelurahan extends Model
+{
+    protected $table = 'kelurahan';
+
+    protected $keyType = 'string';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id', 'kecamatan_id', 'nama'
+    ];
+
+    /**
+     * mendapatkan data kecamatan
+     * @param bool $queryReturn
+     * @return Model|\Illuminate\Database\Eloquent\Relations\BelongsTo|null|object|static
+     */
+    public function getKecamatan($queryReturn = false)
+    {
+        $kecamatan = $this->belongsTo('App\Kecamatan', 'kecamatan_id');
+        if ($queryReturn)
+            return $kecamatan->first();
+        return $kecamatan;
+    }
+
+    /**
+     * mendapatkan data kabupaten
+     * @param bool $queryReturn
+     * @return mixed
+     */
+    public function getKabupaten($queryReturn = false)
+    {
+        return $this->getKecamatan()->getKabupaten($queryReturn);
+    }
+
+    /**
+     * mendapatkan data provinsi
+     * @param bool $queryReturn
+     * @return mixed
+     */
+    public function getProvinsi($queryReturn = false)
+    {
+        return $this->getKabupaten()->getProvinsi($queryReturn);
+    }
+
+    /**
+     * mendapatkan data sportcenter
+     * @param bool $queryReturn
+     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function getSportCenter($queryReturn = false)
+    {
+        $sc = $this->hasMany('App\SportCenter', 'kelurahan_id');
+        if ($queryReturn)
+            return $sc->get();
+        return $sc;
+    }
+}
