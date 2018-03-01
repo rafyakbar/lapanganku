486c3184f0f3b4f3f3653ce62516a987acd03d60
Rafy Aulia Akbar <rafy683@gmail.com>
2018-03-01 08:06:13 +0700

    model member

---
+++ app/Member.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    protected $table = 'member';
+
+    protected $fillable = [
+        'sportcenter_id', 'user_id', 'valid_bulan', 'created_at', 'updated_at'
+    ];
+
+    /**
+     * mendapatkan data sportcenter
+     * @param bool $queryReturn
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Relations\BelongsTo|null|object|static
+     */
+    public function getSportCenter($queryReturn = false)
+    {
+        $data = $this->belongsTo('App\SportCenter', 'sportcenter_id');
+        if ($queryReturn)
+            return $data;
+        return $data->first();
+    }
+
+    /**
+     * mendapatkan data user
+     * @param bool $queryReturn
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Relations\BelongsTo|null|object|static
+     */
+    public function getUser($queryReturn = false)
+    {
+        $data = $this->belongsTo('App\User', 'user_id');
+        if ($queryReturn)
+            return $data;
+        return $data->first();
+    }
+}
