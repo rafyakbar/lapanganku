13d48f6713b738b3ac3a633c7b960cd06b0bd8ca
Rafy Aulia Akbar <rafy683@gmail.com>
2018-03-01 07:36:56 +0700

    model Harga

---
+++ app/Harga.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Harga extends Model
+{
+    protected $table = 'harga';
+
+    protected $fillable = [
+        'sportcenter_id', 'per_jam', 'jam_awal', 'jam_akhir', 'hari'
+    ];
+
+    /**
+     * mendapatkan data sportcenter
+     * @param bool $queryReturn
+     * @return \Illuminate\Database\Eloquent\Model|\Illuminate\Database\Eloquent\Relations\BelongsTo|null|object|static
+     */
+    public function getSportCenter($queryReturn = false)
+    {
+        $data = $this->belongsTo('App\SportCenter', 'sportcenter_id');
+        if ($queryReturn)
+            return $data;
+        return $data->first();
+    }
+
+    /**
+     * mendapatkan data lapangan
+     * @param bool $queryReturn
+     * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function getLapangan($queryReturn = false)
+    {
+        $data = $this->hasMany('App\Lapangan', 'harga_id');
+        if ($queryReturn)
+            return $data;
+        return $data->get();
+    }
+}
