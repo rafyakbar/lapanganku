e341d00fa90706a155408c3cacad36ea89654cd1
Rafy Aulia Akbar <rafy683@gmail.com>
2018-03-02 20:42:55 +0700

    penambahan migrasi harga_lapangan

---
+++ database/migrations/2018_03_02_110223_harga_lapangan.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class HargaLapangan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('harga_lapangan', function (Blueprint $table){
+            $table->bigInteger('harga_id')->unsigned();
+            $table->foreign('harga_id')->references('id')->on('harga')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->bigInteger('lapangan_id')->unsigned();
+            $table->foreign('lapangan_id')->references('id')->on('lapangan')->onUpdate('CASCADE')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
