735e86493a93fbe42a0f44dbbe88d940de453dfa
Rafy Aulia Akbar <rafy683@gmail.com>
2018-03-03 14:27:37 +0700

    HargaLapangan seeder

--- database/seeds/HargaLapanganSeeder.php
+++ database/seeds/HargaLapanganSeeder.php
@@ -6,6 +6,7 @@ use App\Harga;
 use App\Jenis;
 use App\Lapangan;
 use Carbon\Carbon;
+use App\Support\Hari;
 
 class HargaLapanganSeeder extends Seeder
 {
@@ -18,11 +19,31 @@ class HargaLapanganSeeder extends Seeder
     {
         foreach (SportCenter::all() as $sc){
             foreach ($sc->getJenis() as $jenis){
-                Harga::create([
-                    'per_jam' => rand(50,100) * 1000,
-                    'jam_awal' => Carbon::today()->addHours(7)
-
+                $harga_pagi = Harga::create([
+                    'per_jam' => rand(50,75) * 1000,
+                    'jam_awal' => Carbon::today()->addHours(6),
+                    'jam_akhir' => Carbon::today()->addHours(16),
+                    'hari' => implode(';', $h = collect(Hari::ALL)->random(5)->toArray()),
+                    'sportcenter_id' => $sc->id,
+                    'keterangan' => 'Harga lapangan '.$jenis->nama.' pagi'
                 ]);
+                $harga_malam = Harga::create([
+                    'per_jam' => rand(80,150) * 1000,
+                    'jam_awal' => Carbon::today()->addHours(16),
+                    'jam_akhir' => Carbon::today()->addHours(23),
+                    'hari' => implode(';', collect(Hari::ALL)->diff($h)->toArray()),
+                    'sportcenter_id' => $sc->id,
+                    'keterangan' => 'Harga lapangan '.$jenis->nama.' malam'
+                ]);
+                for ($c = 0; $c < rand(1,3); $c++){
+                    $lapangan = Lapangan::create([
+                        'sportcenter_id' => $sc->id,
+                        'jenis_id' => $jenis->id,
+                        'keterangan' => 'Lapangan '.$jenis->nama.' nomor '.($c + 1)
+                    ]);
+                    $lapangan->getHarga(true)->attach($harga_pagi);
+                    $lapangan->getHarga(true)->attach($harga_malam);
+                }
             }
         }
     }
