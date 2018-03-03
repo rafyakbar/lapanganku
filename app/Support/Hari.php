965c9377e8f32a4c6d521ed8fc00689200af76a5
Rafy Aulia Akbar <rafy683@gmail.com>
2018-03-03 14:27:29 +0700

    penambahan class Hari

---
+++ app/Support/Hari.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rafy
+ * Date: 03/03/2018
+ * Time: 07.04
+ */
+
+namespace App\Support;
+
+
+class Hari
+{
+    const MINGGU = 'Sunday';
+
+    const SENIN = 'Monday';
+
+    const SELASA = 'Tuesday';
+
+    const RABU = 'Wednesday';
+
+    const KAMIS = 'Thursday';
+
+    const JUMAT = 'Friday';
+
+    const SABTU = 'Saturday';
+
+    const ALL = [
+        self::MINGGU,
+        self::SENIN,
+        self::SELASA,
+        self::RABU,
+        self::KAMIS,
+        self::JUMAT,
+        self::SABTU
+    ];
+}
