99e90800543a236b375325c75a5780e614ed333d
Rafy Aulia Akbar <rafy683@gmail.com>
2018-03-09 17:10:31 +0700

    update MemberSeeder

--- database/seeds/MemberSeeder.php
+++ database/seeds/MemberSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\User;
 use App\SportCenter;
+use Faker\Factory;
 
 class MemberSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class MemberSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Factory::create();
         foreach (User::getPengunjung() as $user) {
             //untuk mengacak apakah user ini menjadi member atau tidak
             $r = rand(0,1);
@@ -24,7 +26,9 @@ class MemberSeeder extends Seeder
                     //mengisi tabel member (dari hasil many to many)
                     if (rand(0,1)){
                         $user->getMember(true)->attach($sc, [
-                            'valid_bulan' => 12
+                            'valid_bulan' => 12,
+                            'created_at' => $u = $faker->dateTimeBetween('-1 year', 'now'),
+                            'updated_at' => $u
                         ]);
                     }
                 }
