fd44ad9c519eb93985d78819c00f9057d8c5a958
Rafy Aulia Akbar <rafy683@gmail.com>
2018-03-09 17:10:18 +0700

    update PenyewaanSeeder

--- database/seeds/PenyewaanSeeder.php
+++ database/seeds/PenyewaanSeeder.php
@@ -17,18 +17,32 @@ class PenyewaanSeeder extends Seeder
      */
     public function run()
     {
-        foreach (User::all() as $user) {
+        // mengambil user yang role nya PENGUNJUNG
+        foreach (User::getPengunjung() as $user) {
+            // mengambil sportcenter sesuai dengan kecamatan user
             foreach ($user->getKecamatan()->getSportCenter() as $sc) {
+                // mngambil lapangan dari sportcenter
                 foreach ($sc->getLapangan() as $l) {
-                    if (rand(0,1)){
+                    $status = array_random(Status::ALL);
+                    $wm = ($status == Status::BOOKING || $status == Status::DP) ? Carbon::today()->addDays(rand(0,2))->addHours(rand(7, 20))->addMinutes(rand(0, 59)) : Carbon::today()->addDays(rand(-300,0))->addHours(rand(7, 20))->addMinutes(rand(0, 59));
+                    $ws = $wm->copy()->addHours(rand(1, 3));
+                    // mengacak kembali jika telah ada yang menyewa (maks 100 acakan)
+                    $counter = 0;
+                    while (!$l->cekKetersediaan($wm->toDateTimeString(), $ws->toDateTimeString())) {
+                        if ($counter == 100) break;
                         $status = array_random(Status::ALL);
-                        $waktu = ($status == Status::BOOKING || $status == Status::DP) ? Carbon::tomorrow()->addHours(rand(8,21))->addMinutes(rand(0,59)) : Carbon::yesterday()->addHours(rand(8,21))->addMinutes(rand(0,59));
+                        $wm = ($status == Status::BOOKING || $status == Status::DP) ? Carbon::today()->addDays(rand(0,2))->addHours(rand(7, 20))->addMinutes(rand(0, 59)) : Carbon::today()->addDays(rand(-300,0))->addHours(rand(7, 20))->addMinutes(rand(0, 59));
+                        $ws = $wm->copy()->addHours(rand(1, 3));
+                        $counter++;
+                    }
+                    if ($counter < 100){
                         $user->getSewa(true)->attach($l, [
-                            'harga' => $l->getHarga(true)->where('jam_awal', '<=', now()->toTimeString())->where('jam_akhir', '>=', now()->toTimeString())->first()->per_jam,
+                            'harga' => $l->getHarga(true)->where('jam_awal', '<=', $wm->toTimeString())->where('jam_akhir', '>=', $wm->toTimeString())->first()->per_jam,
                             'status' => $status,
-                            'waktu_mulai' => $waktu->toDateTimeString(),
-                            'waktu_selesai' => $waktu->addHours(rand(1,3))->toDateTimeString(),
-                            'registrasi' => false
+                            'waktu_mulai' => $wm->toDateTimeString(),
+                            'waktu_selesai' => $ws->toDateTimeString(),
+                            'registrasi' => ($ws->isSameMonth() && $ws->isSameYear()) ? false : true,
+                            'created_at' => $wm->addHours(rand(3,20))->toDateTimeString()
                         ]);
                     }
                 }
