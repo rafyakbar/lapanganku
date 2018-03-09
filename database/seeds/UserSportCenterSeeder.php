9204dae8eaed232abfd8de73fadfc00e4b7754b5
Rafy Aulia Akbar <rafy683@gmail.com>
2018-03-09 17:10:05 +0700

    update UserSportCenterSeeder

--- database/seeds/UserSportCenterSeeder.php
+++ database/seeds/UserSportCenterSeeder.php
@@ -45,56 +45,58 @@ class UserSportCenterSeeder extends Seeder
             'role' => Role::ROOT
         ]);
 
-        $jatim = Provinsi::where('nama', 'JAWA TIMUR')->first();
         $counter = 0;
-        foreach ($jatim->getKecamatan() as $kecamatan){
-            /**
-             * pemilik, pegawai dan sport center
-             */
-            for($c = 0; $c < rand(1, 3); $c++){
-                //membuat sport center
-                $sc = SportCenter::create([
-                    'kecamatan_id' => $kecamatan->id,
-                    'nama' => $faker->firstName.' Sport Center',
-                    'dir' => '',
-                    'alamat' => $faker->unique()->streetAddress,
-                    'keterangan' => $faker->realText(rand(1000,1750)),
-                    'hari_libur' => implode(';', collect(Hari::ALL)->random(rand(0,2))->toArray())
-                ]);
-                //membuat owner
-                User::create([
-                    'nama' => ($f = $faker->firstName).' '.($l = $faker->lastName),
-                    'email' => strtolower($f).strtolower($l).++$counter.'@lapanganku.com',
-                    'sportcenter_id' => $sc->id,
-                    'kecamatan_id' => $kecamatan->id,
-                    'password' => bcrypt('secret'),
-                    'role' => Role::PEMILIK
-                ]);
-
-                //membuat pegawai
-                for ($i = 0; $i < rand(1, 2); $i++){
+        foreach (Provinsi::where('nama', 'JAWA TIMUR')->first() as $provinsi){
+//            $jatim = Provinsi::where('nama', 'JAWA TIMUR')->first();
+            foreach ($provinsi->getKecamatan() as $kecamatan){
+                /**
+                 * pemilik, pegawai dan sport center
+                 */
+                for($c = 0; $c < rand(1, 3); $c++){
+                    //membuat sport center
+                    $sc = SportCenter::create([
+                        'kecamatan_id' => $kecamatan->id,
+                        'nama' => $faker->firstName.' Sport Center',
+                        'dir' => '',
+                        'alamat' => $faker->streetAddress,
+                        'keterangan' => $faker->realText(rand(1000,1750)),
+                        'hari_libur' => implode(';', collect(Hari::ALL)->random(rand(0,2))->toArray())
+                    ]);
+                    //membuat owner
                     User::create([
                         'nama' => ($f = $faker->firstName).' '.($l = $faker->lastName),
                         'email' => strtolower($f).strtolower($l).++$counter.'@lapanganku.com',
                         'sportcenter_id' => $sc->id,
                         'kecamatan_id' => $kecamatan->id,
                         'password' => bcrypt('secret'),
-                        'role' => Role::PEGAWAI
+                        'role' => Role::PEMILIK
+                    ]);
+
+                    //membuat pegawai
+                    for ($i = 0; $i < rand(1, 2); $i++){
+                        User::create([
+                            'nama' => ($f = $faker->firstName).' '.($l = $faker->lastName),
+                            'email' => strtolower($f).strtolower($l).++$counter.'@lapanganku.com',
+                            'sportcenter_id' => $sc->id,
+                            'kecamatan_id' => $kecamatan->id,
+                            'password' => bcrypt('secret'),
+                            'role' => Role::PEGAWAI
+                        ]);
+                    }
+                }
+
+                /**
+                 * membuat pengunjung
+                 */
+                for ($c = 0; $c < rand(5, 10); $c++){
+                    User::create([
+                        'nama' => ($f = $faker->firstName).' '.($l = $faker->lastName),
+                        'email' => strtolower($f).strtolower($l).++$counter.'@lapanganku.com',
+                        'kecamatan_id' => $kecamatan->id,
+                        'password' => bcrypt('secret'),
+                        'role' => Role::PENGUNJUNG
                     ]);
                 }
-            }
-
-            /**
-             * membuat pengunjung
-             */
-            for ($c = 0; $c < rand(2, 4); $c++){
-                User::create([
-                    'nama' => ($f = $faker->firstName).' '.($l = $faker->lastName),
-                    'email' => strtolower($f).strtolower($l).++$counter.'@lapanganku.com',
-                    'kecamatan_id' => $kecamatan->id,
-                    'password' => bcrypt('secret'),
-                    'role' => Role::PENGUNJUNG
-                ]);
             }
         }
     }
