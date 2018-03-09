6dd2a4c043dc2fea4e15c11addbda338bad5da49
Rafy Aulia Akbar <rafy683@gmail.com>
2018-03-09 17:10:56 +0700

    update MemberLapanganSeeder

--- database/seeds/MemberLapanganSeeder.php
+++ database/seeds/MemberLapanganSeeder.php
@@ -15,20 +15,24 @@ class MemberLapanganSeeder extends Seeder
      */
     public function run()
     {
+        //mengambil data sportcenter
         foreach (SportCenter::all() as $sc){
+            //mengambil data member per sportcenter
             foreach ($sc->getMember() as $user){
                 $lapangan = $sc->getLapangan()->random();
                 $jumbulan = rand(5,8);
                 $waktu = Carbon::parse($user->pivot->created_at)->addDays(rand(1,7))->addHours(rand(6,20));
                 $mulai = Carbon::today()->addHours(rand(6,18));
                 $hari = implode(';', collect(Hari::ALL)->diff(explode(';', $sc->hari_libur))->random($jumhari = rand(2,3))->toArray());
+                //membuat penyewaan lapangan secara rutin untuk member
                 Member::find($user->pivot->id)->getLapangan(true)->attach($lapangan, [
                     'jumlah_bulan' => $jumbulan,
                     'harga' => rand(60,90) * 1000 * $jumbulan * $jumhari * 4,
                     'waktu_mulai' => $mulai->toTimeString(),
                     'waktu_selesai' => $mulai->addHours(rand(1,3))->toTimeString(),
                     'hari' => $hari,
-                    'created_at' => $waktu->toDateTimeString()
+                    'created_at' => $waktu->toDateTimeString(),
+                    'updated_at' => $waktu->toDateTimeString()
                 ]);
             }
         }
