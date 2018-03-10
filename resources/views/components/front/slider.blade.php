d1128f3c2ebd3fc316726dd7ec2b88ae1e5c6d56
Rafy Aulia Akbar <rafy683@gmail.com>
2018-03-10 09:25:18 +0700

    komponen slider

---
+++ resources/views/components/front/slider.blade.php
@@ -0,0 +1,81 @@
+<!--=========== BEGIN SLIDER SECTION ================-->
+<section id="sliderArea">
+    <!-- Start slider wrapper -->
+    <div class="top-slider">
+        <!-- Start First slide -->
+        <div class="top-slide-inner">
+            <div class="slider-img">
+                <img src="{{ asset('front/images/14.jpg') }}" alt="">
+            </div>
+            <div class="slider-text">
+                <h2>An <strong>Excellent Theme</strong> for your Health Related Project</h2>
+                <p><strong>WpF Medinova</strong> Theme suitable for any health and medical related projets or business</p>
+                <div class="readmore_area">
+                    <a data-hover="Read More" href="#"><span>Read More</span></a>
+                </div>
+            </div>
+        </div>
+        <!-- End First slide -->
+
+        <!-- Start 2nd slide -->
+        <div class="top-slide-inner">
+            <div class="slider-img">
+                <img src="{{ asset('front/images/15.jpg') }}" alt="">
+            </div>
+            <div class="slider-text">
+                <h2><strong>Best Template</strong> For your Medical Website</h2>
+                <p><strong>WpF Medinova</strong> Theme suitable for any health and medical related projets or business</p>
+                <div class="readmore_area">
+                    <a data-hover="Read More" href="#"><span>Read More</span></a>
+                </div>
+            </div>
+        </div>
+        <!-- End 2nd slide -->
+
+        <!-- Start Third slide -->
+        <div class="top-slide-inner">
+            <div class="slider-img">
+                <img src="{{ asset('front/images/7.jpg') }}" alt="">
+            </div>
+            <div class="slider-text">
+                <h2>An <strong>Excellent Theme</strong> for your Health Related Project</h2>
+                <p><strong>WpF Medinova</strong> Theme suitable for any health and medical related projets or business</p>
+                <div class="readmore_area">
+                    <a data-hover="Read More" href="#"><span>Read More</span></a>
+                </div>
+            </div>
+        </div>
+        <!-- End Third slide -->
+
+        <!-- Start Fourth slide -->
+        <div class="top-slide-inner">
+            <div class="slider-img">
+                <img src="{{ asset('front/images/12.jpg') }}" alt="">
+            </div>
+            <div class="slider-text">
+                <h2><strong>Best Template</strong> For your Medical Website</h2>
+                <p><strong>WpF Medinova</strong> Theme suitable for any health and medical related projets or business</p>
+                <div class="readmore_area">
+                    <a data-hover="Read More" href="#"><span>Read More</span></a>
+                </div>
+            </div>
+        </div>
+        <!-- End Fourth slide -->
+
+        <!-- Start Fifth slide -->
+        <div class="top-slide-inner">
+            <div class="slider-img">
+                <img src="{{ asset('front/images/9.jpg') }}" alt="">
+            </div>
+            <div class="slider-text">
+                <h2>An <strong>Excellent Theme</strong> for your Health Related Project</h2>
+                <p><strong>WpF Medinova</strong> Theme suitable for any health and medical related projets or business</p>
+                <div class="readmore_area">
+                    <a data-hover="Read More" href="#"><span>Read More</span></a>
+                </div>
+            </div>
+        </div>
+        <!-- End Fifth slide -->
+    </div><!-- /top-slider -->
+</section>
+<!--=========== END SLIDER SECTION ================-->
