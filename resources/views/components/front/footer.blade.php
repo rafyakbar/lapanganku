1036c3e2787e6711a6fe3ad118540f87ad3eb292
Rafy Aulia Akbar <rafy683@gmail.com>
2018-03-10 09:24:47 +0700

    footer

---
+++ resources/views/components/front/footer.blade.php
@@ -0,0 +1,86 @@
+<!--=========== Start Footer SECTION ================-->
+<footer id="footer">
+    <!-- Start Footer Top -->
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-3">
+                    <div class="single-footer-widget">
+                        <div class="section-heading">
+                            <h2>About Us</h2>
+                            <div class="line"></div>
+                        </div>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3">
+                    <div class="single-footer-widget">
+                        <div class="section-heading">
+                            <h2>Our Service</h2>
+                            <div class="line"></div>
+                        </div>
+                        <ul class="footer-service">
+                            <li><a href="#"><span class="fa fa-check"></span>Service 1</a></li>
+                            <li><a href="#"><span class="fa fa-check"></span>Service 2</a></li>
+                            <li><a href="#"><span class="fa fa-check"></span>Service 3</a></li>
+                            <li><a href="#"><span class="fa fa-check"></span>Service 4</a></li>
+                            <li><a href="#"><span class="fa fa-check"></span>Service 5</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3">
+                    <div class="single-footer-widget">
+                        <div class="section-heading">
+                            <h2>Tags</h2>
+                            <div class="line"></div>
+                        </div>
+                        <ul class="tag-nav">
+                            <li><a href="#">Dental</a></li>
+                            <li><a href="#">Surgery</a></li>
+                            <li><a href="#">Pediatric</a></li>
+                            <li><a href="#">Cardiac</a></li>
+                            <li><a href="#">Ophthalmology</a></li>
+                            <li><a href="#">Diabetes</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3">
+                    <div class="single-footer-widget">
+                        <div class="section-heading">
+                            <h2>Contact Info</h2>
+                            <div class="line"></div>
+                        </div>
+                        <p>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
+                        <address class="contact-info">
+                            <p><span class="fa fa-home"></span>305 Intergraph Way
+                                Madison, AL 35758, USA</p>
+                            <p><span class="fa fa-phone"></span>1.256.730.2000</p>
+                            <p><span class="fa fa-envelope"></span>info@wpfmedinova.com</p>
+                        </address>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Start Footer Middle -->
+    <div class="footer-middle">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <div class="footer-copyright">
+                        <p>&copy; Copyright {{ \Carbon\Carbon::now()->year }} LapanganKu</p>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <div class="footer-social">
+                        <a href="#"><span class="fa fa-facebook"></span></a>
+                        <a href="#"><span class="fa fa-twitter"></span></a>
+                        <a href="#"><span class="fa fa-google-plus"></span></a>
+                        <a href="#"><span class="fa fa-linkedin"></span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--=========== End Footer SECTION ================-->
