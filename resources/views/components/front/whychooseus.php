8146adb533f66f0c425e03aea2af502d750e6d14
Rafy Aulia Akbar <rafy683@gmail.com>
2018-03-10 09:25:38 +0700

    komponen whychooseus

---
+++ resources/views/components/front/whychooseus.php
@@ -0,0 +1,73 @@
+<!--=========== BEGAIN Why Choose Us SECTION ================-->
+<section id="whychooseSection">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <div class="service-area">
+                    <!-- Start Service Title -->
+                    <div class="section-heading">
+                        <h2>Why Choose Us</h2>
+                        <div class="line"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-12">
+                <div class="row">
+                    <div class="col-lg-5 col-md-6 col-sm-6 col-xs-12">
+                        <div class="whyChoose-left">
+                            <div class="whychoose-slider">
+                                <div class="whychoose-singleslide">
+                                    <img src="{{ asset('front/images/choose-us-img1.jpg') }}" alt="img">
+                                </div>
+                                <div class="whychoose-singleslide">
+                                    <img src="{{ asset('front/images/choose-us-img2.jpg') }}" alt="img">
+                                </div>
+                                <div class="whychoose-singleslide">
+                                    <img src="{{ asset('front/images/choose-us-img3.jpg') }}" alt="img">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-md-6 col-sm-6 col-xs-12">
+                        <div class="whyChoose-right">
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="#" class="media-icon">
+                                        <span class="fa fa-hospital-o"></span>
+                                    </a>
+                                </div>
+                                <div class="media-body">
+                                    <h4 class="media-heading">Great Infrastructure</h4>
+                                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="#" class="media-icon">
+                                        <span class="fa fa-user-md"></span>
+                                    </a>
+                                </div>
+                                <div class="media-body">
+                                    <h4 class="media-heading">Qualified Doctors</h4>
+                                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <div class="media-left">
+                                    <a href="#" class="media-icon">
+                                        <span class="fa fa-ambulance"></span>
+                                    </a>
+                                </div>
+                                <div class="media-body">
+                                    <h4 class="media-heading">Emergency Support</h4>
+                                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--=========== END Why Choose Us SECTION ================-->
